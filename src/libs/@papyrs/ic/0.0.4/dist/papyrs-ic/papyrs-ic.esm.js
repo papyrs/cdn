bc5e0e4005815ccd1f46176e91d271fe11513bcf
peterpeterparker <david.dalbusco@outlook.com>
2022-04-14 08:38:14 +0200

    feat: remove unused ic version

--- src/libs/@papyrs/ic/0.0.4/dist/papyrs-ic/papyrs-ic.esm.js
+++
@@ -1,1 +0,0 @@
-import{p as n,b as s}from"./p-0adadf10.js";(()=>{const s=import.meta.url,i={};return""!==s&&(i.resourcesUrl=new URL(".",s).href),n(i)})().then((n=>s([["p-bc27808d",[[4,"deckgo-ic-signin",{i18n:[16],config:[16],signInSuccess:[16],signInError:[16],signInInProgress:[32]}]]]],n)));
