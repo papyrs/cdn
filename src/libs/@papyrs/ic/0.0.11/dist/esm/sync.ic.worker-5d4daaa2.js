8a92f1ded1fbbca9b92298c9d1213c16eeee6798
peterpeterparker <david.dalbusco@outlook.com>
2022-06-12 18:09:26 +0200

    feat: ic v0.0.11 (for Excalidraw / data-src support)

---
+++ src/libs/@papyrs/ic/0.0.11/dist/esm/sync.ic.worker-5d4daaa2.js
@@ -0,0 +1,12 @@
+import { p as createWorker } from './auth.providers-965ac13f.js';
+import './index-9daac24f.js';
+
+const workerName = 'sync.ic.worker';
+const workerMsgId = 'stencil.sync.ic.worker';
+const workerPath = /*@__PURE__*/new URL('sync.ic.worker-0749029b.js', import.meta.url).href;
+const blob = new Blob(['importScripts("' + workerPath + '")'], { type: 'text/javascript' });
+const url = URL.createObjectURL(blob);
+const worker = /*@__PURE__*/createWorker(url, workerName, workerMsgId);
+URL.revokeObjectURL(url);
+
+export { worker, workerMsgId, workerName, workerPath };
