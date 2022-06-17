a75400c184402cc6dfe09455b4ba697344a09b7e
peterpeterparker <david.dalbusco@outlook.com>
2022-06-17 17:48:45 +0200

    feat: ic v0.0.12

---
+++ src/libs/@papyrs/ic/0.0.12/dist/esm/user.ic.worker-2b3841e5.js
@@ -0,0 +1,12 @@
+import { p as createWorker } from './auth.providers-5aeebc0c.js';
+import './index-9daac24f.js';
+
+const workerName = 'user.ic.worker';
+const workerMsgId = 'stencil.user.ic.worker';
+const workerPath = /*@__PURE__*/new URL('user.ic.worker-d8461be3.js', import.meta.url).href;
+const blob = new Blob(['importScripts("' + workerPath + '")'], { type: 'text/javascript' });
+const url = URL.createObjectURL(blob);
+const worker = /*@__PURE__*/createWorker(url, workerName, workerMsgId);
+URL.revokeObjectURL(url);
+
+export { worker, workerMsgId, workerName, workerPath };
