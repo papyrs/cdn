91ec5f4bf3ae4b9bc8f58061998df149219d617c
peterpeterparker <david.dalbusco@outlook.com>
2022-07-08 10:33:44 +0200

    feat: ic v0.0.15-1

---
+++ src/libs/@papyrs/ic/0.0.15-1/dist/esm/idle.ic.worker-60165fe2.js
@@ -0,0 +1,12 @@
+import { p as createWorker } from './auth.providers-3f7ade85.js';
+import './index-a398e020.js';
+
+const workerName = 'idle.ic.worker';
+const workerMsgId = 'stencil.idle.ic.worker';
+const workerPath = /*@__PURE__*/new URL('idle.ic.worker-8fccb963.js', import.meta.url).href;
+const blob = new Blob(['importScripts("' + workerPath + '")'], { type: 'text/javascript' });
+const url = URL.createObjectURL(blob);
+const worker = /*@__PURE__*/createWorker(url, workerName, workerMsgId);
+URL.revokeObjectURL(url);
+
+export { worker, workerMsgId, workerName, workerPath };
