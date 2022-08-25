b6a63616d39efa74ceaf686ac5c53a11cee3cea3
peterpeterparker <david.dalbusco@outlook.com>
2022-08-25 09:48:44 +0200

    feat: ic v0.0.17

---
+++ src/libs/@papyrs/ic/0.0.17/dist/esm/sync.ic.worker-f6024fc3.js
@@ -0,0 +1,12 @@
+import { o as createWorker } from './auth.providers-33f1da54.js';
+import './index-a398e020.js';
+
+const workerName = 'sync.ic.worker';
+const workerMsgId = 'stencil.sync.ic.worker';
+const workerPath = /*@__PURE__*/new URL('sync.ic.worker-a4379786.js', import.meta.url).href;
+const blob = new Blob(['importScripts("' + workerPath + '")'], { type: 'text/javascript' });
+const url = URL.createObjectURL(blob);
+const worker = /*@__PURE__*/createWorker(url, workerName, workerMsgId);
+URL.revokeObjectURL(url);
+
+export { worker, workerMsgId, workerName, workerPath };
---
+++ src/libs/@papyrs/ic/0.0.17/dist/esm/user.ic.worker-6bf91bea.js
@@ -0,0 +1,12 @@
+import { o as createWorker } from './auth.providers-33f1da54.js';
+import './index-a398e020.js';
+
+const workerName = 'user.ic.worker';
+const workerMsgId = 'stencil.user.ic.worker';
+const workerPath = /*@__PURE__*/new URL('user.ic.worker-6ff4bce3.js', import.meta.url).href;
+const blob = new Blob(['importScripts("' + workerPath + '")'], { type: 'text/javascript' });
+const url = URL.createObjectURL(blob);
+const worker = /*@__PURE__*/createWorker(url, workerName, workerMsgId);
+URL.revokeObjectURL(url);
+
+export { worker, workerMsgId, workerName, workerPath };
