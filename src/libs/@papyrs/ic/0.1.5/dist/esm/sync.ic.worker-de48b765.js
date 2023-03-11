96c04e99cd8a5b98546d2139541967ee5839b233
David Dal Busco <david.dalbusco@outlook.com>
2023-03-11 16:38:06 +0100

    build: papyrs/ic v0.1.5

---
+++ src/libs/@papyrs/ic/0.1.5/dist/esm/sync.ic.worker-de48b765.js
@@ -0,0 +1,15 @@
+import { f as createWorker } from './auth.providers-8d064017.js';
+import './auth.constants-1b8a7422.js';
+import './actor-56e8d250.js';
+import './compat-029eb582.js';
+import './index-ec2f5921.js';
+
+const workerName = 'sync.ic.worker';
+const workerMsgId = 'stencil.sync.ic.worker';
+const workerPath = /*@__PURE__*/new URL('sync.ic.worker-96cb59a1.js', import.meta.url).href;
+const blob = new Blob(['importScripts("' + workerPath + '")'], { type: 'text/javascript' });
+const url = URL.createObjectURL(blob);
+const worker = /*@__PURE__*/createWorker(url, workerName, workerMsgId);
+URL.revokeObjectURL(url);
+
+export { worker, workerMsgId, workerName, workerPath };
